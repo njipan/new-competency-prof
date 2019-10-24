aa87bb15e9c7987e56b5ad2446c298e7a39d49d8
njipan <pnjikrna@gmail.com>
2019-10-24 20:31:27 +0700

    fix: symlink

--- newStaff/view/competency-profiling.js
+++ newStaff/view/competency-profiling.js
@@ -1,1 +1,32 @@
-C:/xampp-56/htdocs/BM5/newStaff/view/competency-profiling.js
+var sv, objectToString;
+var subView = {
+    rel: 'content',
+    title: 'Competency Profiling',
+    onLoaded : function(){
+        sv = this;
+        sv.prepare();
+    },
+    prepare: function() {
+        $(".page-heading h1").text(this.title);
+        var $breadCrumbs = $("#BC_Caption");
+        while ($breadCrumbs.children().length > 1)
+          $breadCrumbs
+            .children()
+            .last()
+            .remove();
+        $breadCrumbs.append('<li><a href="#">Competency Profiling</a></li>');
+        $breadCrumbs.append("<li>" + this.title + "</li>");
+    },
+};
+objectToString = function(data={}){
+    let result = '';
+    let i = 0;
+    for(let key in data){
+        i++;
+        result += `${key}=${data[key]}`;
+        if(Object.keys(data).length != i){
+            result += "&";
+        }
+    }
+    return result.trim();
+}
